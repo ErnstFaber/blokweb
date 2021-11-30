2270b53082ad532d61e38645bc01aae035b6771b
ErnstFaber <ernstfabero@gmail.com>
2021-11-30 14:36:45 +0100

    init

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+var hamburgerIcon = document.querySelector("header nav ul li:first-of-type");
+var hamburgerMenu = document.querySelector("header section")
+
+console.log(hamburgerIcon);
+console.log(hamburgerMenu);
+
+hamburgerIcon.addEventListener("click", function(){
+    hamburgerMenu.classList.toggle("aan")
+})
